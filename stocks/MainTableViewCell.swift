ba7da13e1f6ed06f6b6d574551e2849c2521fd8e
Kirill Kostarev <kirill.kostarev.2017@gmail.com>
2021-02-19 19:56:28 +0300

    Design of main menu

--- stocks/MainTableViewCell.swift
+++ stocks/MainTableViewCell.swift
@@ -8,7 +8,43 @@
 import UIKit
 
 class MainTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var stockImage: UIImageView! {
+        didSet {
+            stockImage.layer.masksToBounds = true
+            stockImage.layer.cornerRadius = 10.0
+        }
+    }
+    @IBOutlet weak var stockName: UILabel! {
+        didSet {
+            stockName.font = UIFont(name: "Montserrat-Bold", size: 18)
+            stockName.textColor = .black
+        }
+    }
+    @IBOutlet weak var stockCompanyName: UILabel! {
+        didSet {
+                stockCompanyName.font = UIFont(name: "Montserrat-Medium", size: 14)
+                stockCompanyName.textColor = .black
+            }
+        }
+    @IBOutlet weak var favouriteButton: UIButton! {
+        didSet {
+            favouriteButton.titleLabel?.text = nil
+            favouriteButton.setImage(UIImage(named: "favouriteNonSelected"), for: .normal)
+        }
+    }
+    @IBOutlet weak var stockPrice: UILabel! {
+        didSet {
+            stockPrice.font = UIFont(name: "Montserrat-Bold", size: 18)
+            stockPrice.textColor = .black
+        }
+    }
+    @IBOutlet weak var stockInfo: UILabel! {
+        didSet {
+            stockInfo.font = UIFont(name: "Montserrat-Medium", size: 14)
+            stockInfo.textColor = .black
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,5 +55,6 @@ class MainTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    @IBAction func addToFavouriteStock(_ sender: UIButton) {
+    }
 }
